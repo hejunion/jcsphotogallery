2ad499e90a4f069f9a7ce2710b1fc008148f3b1b
dan.cioi <dan.cioi@7f93270c-6da3-7ff0-9b57-724b90f03c11>
2013-01-07 21:12:19 +0000

    working on version 1.1.2beta

--- trunk/jcsphotogallery/src/net/dancioi/jcsphotogallery/app/view/AppPanelLeft.java
+++ trunk/jcsphotogallery/src/net/dancioi/jcsphotogallery/app/view/AppPanelLeft.java
@@ -37,8 +37,6 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.TransferHandler;
-import javax.swing.event.TreeSelectionEvent;
-import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
